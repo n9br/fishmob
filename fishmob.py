b6912c7ce66ee01189e5725025c3981c025469f3
N9br <andre.neugebauer@gmail.com>
2016-11-25 22:11:03 +0100

    initial simple code

---
+++ fishmob.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import ConfigParser
+
+# configfile = "/etc/fishmob.cfg"
+configfile = "/etc/fishmob.cfg"
+
+
+
+
+parser = argparse.ArgumentParser(description="program controlling fishmob \n specify Action, PIN \n uses configfile in /etc")
+parser.add_argument("action", help="read = read sensor, or control control a device", type=str)
+parser.add_argument("devicename", help="Name des Sensors oder des Geräts", type=str)
+parser.add_argument("--help, ")
+args = parser.parse_args()
+
+# initialize and read config
+# Config wird eine Liste von Dictionaries
+# ansprechbar via Config.items[Sectionname]
+Config=ConfigParser.ConfigParser()
+Config
+Config.read(configfile)
+
+# Sensor = key/value dict of properties
+Sensor = Config.items(args.devicename)
+
+print Sensor
+
+
