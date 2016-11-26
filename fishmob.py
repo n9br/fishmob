b862a4c1dc6b4f00d6237f3110ceeaeb79e39c34
n9br <andre.neugebauer@gmail.com>
2016-11-26 12:58:56 +0100

    from raspi# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.# On branch master# Changes to be committed:#	modified:   fishmob.py#

--- fishmob.py
+++ fishmob.py
@@ -3,6 +3,7 @@
 
 import argparse
 import ConfigParser
+import RPi.GPIO as GPIO ## Import GPIO library
 
 # configfile = "/etc/fishmob.cfg"
 configfile = "/etc/fishmob.cfg"
@@ -11,8 +12,8 @@ configfile = "/etc/fishmob.cfg"
 
 
 parser = argparse.ArgumentParser(description="program controlling fishmob \n specify Action, PIN \n uses configfile in /etc")
-parser.add_argument("action", help="read = read sensor, or control control a device", type=str)
-parser.add_argument("devicename", help="Name des Sensors oder des Geräts", type=str)
+parser.add_argument("action", help="read = read sensor, or control = control a device", type=str)
+parser.add_argument("devicename", help="Name des Sensors oder des Geräts lt Section des config file", type=str)
 parser.add_argument("--help, ")
 args = parser.parse_args()
 
@@ -24,8 +25,14 @@ Config
 Config.read(configfile)
 
 # Sensor = key/value dict of properties
-Sensor = Config.items(args.devicename)
-
+lSensor = Config.items(args.devicename)
+Sensor = dict(lSensor)
 print Sensor
 
+# initialize mode; using  
+GPIO.setmode(GPIO.BOARD)
 
+# set direction of PINs (in/out)
+pin = int(Sensor['pin'])
+print Sensor['pin']
+GPIO.setup(pin, GPIO.OUT) 
