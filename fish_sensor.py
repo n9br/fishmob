171246002c44cf8787e38e9ed120bdc726689290
N9br <andre.neugebauer@gmail.com>
2016-12-08 19:08:20 +0100

    first changes to master after branching

--- fish_sensor.py
+++ fish_sensor.py
@@ -4,19 +4,18 @@
 import argparse
 import ConfigParser
 import datetime
-import RPi.GPIO as GPIO ## Import GPIO library
 import sqlite3 as lite
 import sys
 
 
 parser = argparse.ArgumentParser(description="getting sensor data for fishmob \n specify sensor in configfile")
-parser.add_argument("devicename", help="Name des Sensors lt Section des config file", type=str)
+# parser.add_argument("devicename", help="Name des Sensors lt Section des config file", type=str)
 parser.add_argument("--help, ")
 args = parser.parse_args()
 
 now = datetime.datetime.now()
 # configfile = "/etc/fishmob.cfg"
-configfile = "/etc/fishmob.cfg"
+configfile = "/etc/fishmob/sensors.cfg"
 dbname = str(now.year)+str(now.month)+'.db'
 
 
@@ -28,16 +27,5 @@ Config
 Config.read(configfile)
 
 # Sensor = key/value dict of properties
-lSensor = Config.items(args.devicename)
-Sensor = dict(lSensor)
-print Sensor
-
-# initialize mode; using  
-GPIO.setmode(GPIO.BOARD)
-
-# set direction of PINs (in/out)
-pin = int(Sensor['pin'])
-print Sensor['pin']
-GPIO.setup(pin, GPIO.OUT)
-
-
+for section in Config.sections():
+    print Config.items(section)
