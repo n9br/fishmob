42595bf28b0081c6cd50efa726f0b931c7aaa5c9
N9br <andre.neugebauer@gmail.com>
2017-01-15 20:30:12 +0100

    fixed date storage in db

--- fmsensor.py
+++ fmsensor.py
@@ -8,6 +8,7 @@ import datetime
 # import RPi.GPIO as GPIO ## Import GPIO library
 import sqlite3 as lite
 import sys
+import os
 
 # con None
 configfile = "/etc/fishmob/sensors.cfg"
@@ -19,9 +20,14 @@ parser.add_argument("--help, ")
 args = parser.parse_args()
 
 now = datetime.datetime.now()
-dbname = str(now.year)+str(now.month)+'.db'
-sDate = str(now.date())
-print sDate
+
+if not (os.access('/var/lib/fishmob', os.W_OK)):
+    print "pls execute 'sudo mkdir /var/lib/fishmob && sudo chown pi /var/lib/fishmob'"
+    sys.exit(1)
+
+dbname = str(now.year)+str(now.month).zfill(2)+'.db'
+# sDate = str(now.date())
+# print sDate
 
 
 # initialize and read config
@@ -42,10 +48,10 @@ for section in Config.sections():
     if not platform.machine() == 'armv7l':
         import random
         random.seed()
-        degrees = random.uniform(-5,30)
+        degrees = str(random.uniform(-5,30))
         hectopascals = random.uniform(1000,1020)
         humidity = random.uniform(30,99)
-        print 'Temp      = {0:0.3f} deg C'.format(degrees)
+        print 'Temp      = {0:0.3f} deg C'.format(float(degrees))
         print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
         print 'Humidity  = {0:0.2f} %'.format(humidity)
 #        next;
@@ -64,13 +70,17 @@ for section in Config.sections():
             print 'Temp      = {0:0.3f} deg C'.format(degrees)
             print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
             print 'Humidity  = {0:0.2f} %'.format(humidity)
+
 #   create table if non-existent
     cur.execute('CREATE TABLE IF NOT EXISTS {tn} \
-        (date text, hour int, min int, hum real, press real, temp real )'.format(tn=section))
+        (date date, hour int, min int, hum real, press real, temp real )'.format(tn=section))
+#        (date text, hour int, min int, hum real, press real, temp real )'.format(tn=section))
+
 
 #   insert Data
-    cur.execute("INSERT INTO {tn} VALUES ({dt},{hr},{min},{hum},{press},{temp})".\
-        format(tn=section, dt=sDate, hr=now.hour, min=now.minute, hum=humidity, press=hectopascals, temp=degrees))
+    cur.execute("INSERT INTO {tn} VALUES (date('now','localtime'),{hr},{min},{hum},{press},{temp})".\
+        format(tn=section, hr=now.hour, min=now.minute, hum=humidity, press=hectopascals, temp=degrees))
+#        format(tn=section, dt=sDate, hr=now.hour, min=now.minute, hum=humidity, press=hectopascals, temp=degrees))
 #    cur.execute("INSERT INTO TempSensor1 VALUES ('2016-12-21','08','07')")
 
 #    try:
